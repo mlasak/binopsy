c827ab499454b54b59a9580bd4bfcb82b92f4af8
Felix Böhm <me@feedic.com>
2016-03-13 13:24:45 +0100

    more methods\!

--- combined.js
+++ combined.js
@@ -41,23 +41,42 @@ Bin.prototype.sizeOf = function(obj){
   return this.serializer.sizeOf(obj)
 }
 
-;['string', 'buffer', 'array', 'choice', 'nest']
+;['string', 'buffer', 'array', 'nest']
   .concat(Object.keys(PRIMITIVE_TYPES).map(Function.prototype.call, String.prototype.toLowerCase))
   //TODO bitfields
   .forEach(function(name){
     Bin.prototype[name] = function(varName, options){
-      //FIXME only handles parser instances
       if(options && options.type instanceof Bin){
-        options = {__proto__: options, type: options.type.parser}
+        var parserOpts = {__proto__: options, type: options.type.parser}
+        var serializerOpts = {__proto__: options, type: options.type.serializer}
       }
-      this.parser[name](varName, options)
-      this.serializer[name] && this.serializer[name](varName, options) //TODO
+      this.parser[name](varName, parserOpts || options)
+      this.serializer[name] && this.serializer[name](varName, serializerOpts || options) //TODO
       return this
     }
   })
 
+Bin.prototype.choice = function(varName, options){
+  var choices = options.choices
+  var parserChoices = {}
+  var serializerChoices = {}
+
+  for(var key in choices){
+    if(typeof choices[key] === "object"){
+      parserChoices[key] = choices[key].parser
+      serializerChoices[key] = choices[key].serializer
+    } else {
+      parserChoices[key] = serializerChoices[key] = choices[key]
+    }
+  }
+
+  this.parser.choice(varName, {__proto__:options, choices: parserChoices})
+  this.serializer.choice(varName, {__proto__:options, choices: serializerChoices})
+  return this
+}
+
 Bin.prototype.create = function(constructorFn){
-  this.parser.create(buffer, callback)
+  this.parser.create(constructorFn)
   return this
 }
 
@@ -74,9 +93,20 @@ Object.keys(PRIMITIVE_TYPES)
     var name = primitiveName.slice(0, -2).toLowerCase()
 
     Bin.prototype[name] = function(varName, options){
-      return Serializer.prototype[name + this.endian]
+      return this[name + this.endian](varName, options)
     }
   })
+
+for(var i = 1; i < 25; i++){
+  (function(i){
+    var name = 'bit' + i
+    Bin.prototype[name] = function(varName, options){
+      this.parser[name](varName, options)
+      //TODO implement in serializer
+      return this
+    }
+  }(i))
+}
 
 //copied from binary_parser.js
 Bin.prototype.endianess = function(endianess) {
--- index.js
+++ index.js
@@ -1,85 +1,5 @@
 module.exports = Serializer
 
-function Serializer(){
-  this.sizeOf = function(obj){ return 0 }
-  this.writeFunc = function(obj, buf){ return 0 }
-  this.vars = new Set()
-}
-
-Serializer.prototype.serialize = function(obj, buf){
-  if(!Buffer.isBuffer(buf)){
-    var size = this.sizeOf(obj)
-    buf = new Buffer(size)
-  }
-
-  this.writeFunc(obj, buf, 0)
-
-  return buf
-}
-
-Serializer.prototype.string = function(varName, options){
-  this._addVar(varName)
-
-  var getVar = this._getVarFunc(varName, options)
-
-  var sizeOf = this.sizeOf
-  var writeFunc = this.writeFunc
-
-  var encoding = options.encoding || 'utf8'
-
-  if(options.zeroTerminated){
-    this.sizeOf = function(obj){
-      return getVar(obj).length + 1 + sizeOf(obj)
-    }
-
-    this.writeFunc = function(obj, buf){
-      var offset = writeFunc(obj, buf);
-      var val = getVar(obj)
-
-      return offset + buf.write(val + '\0', offset, val.length + 1, encoding)
-    }
-
-    return
-  }
-
-  if(options.length){
-    var length = options.length
-
-    var getLength = typeof options.length === 'number' ?
-                      function(obj){ return length } :
-                    typeof options.length === 'string' ?
-                      function(obj){ return obj[length] } :
-                      function(obj){ return length(obj) }
-
-    this.sizeOf = function(obj){
-      return getLength(obj) + sizeOf(obj)
-    }
-
-    var fixVal = options.stripNull ?
-              function(val, len){
-                if(val.length > len) throw new Error('string too long')
-                while(val.length < len){
-                  val = val + '\0'
-                }
-                return val
-              } : function(val, len){
-                //if(val.length !== len) throw new Error('string length doesn\'t match')
-                return val
-              }
-
-    this.writeFunc = function(obj, buf){
-      var offset = writeFunc(obj, buf)
-      var len = getLength(obj)
-      var val = fixVal(getVar(obj), len)
-      return offset + buf.write(val, offset, len, encoding)
-    }
-
-    return
-  }
-
-  throw new Error('.string needs either a length or a zero-terminated string')
-}
-
 //copied from binary_parser.js
 const PRIMITIVE_TYPES = {
     'UInt8'    : 1,
@@ -98,12 +18,256 @@ const PRIMITIVE_TYPES = {
     'DoubleBE' : 8
 }
 
+const TYPE_FUNCTIONS = Object.keys(PRIMITIVE_TYPES).reduce(function(obj, key){
+  var writeKey = 'write' + key
+  obj[key.toLowerCase()] = {
+    sizeOf: function(){
+      return PRIMITIVE_TYPES[key]
+    },
+    writeFunc: function(val, buf){
+      return buf[writeKey](val)
+    }
+  }
+  return obj
+}, {})
+
+function getType(type){
+  if(typeof type === "string" && type in TYPE_FUNCTIONS){
+    return TYPE_FUNCTIONS[type]
+  } else if(!(type instanceof Serializer)){
+    throw new Error("type needs to be a serializer or a primitive")
+  }
+
+  return type
+}
+
+function getGetVarFunc(varName){
+  return function(obj){
+    if(!(varName in obj)) throw new Error('var not found')
+    return obj[varName]
+  }
+}
+
+function Serializer(){
+  this.sizeOf = function(obj){ return 0 }
+  this.writeFunc = function(obj, buf){ return 0 }
+  this.vars = new Set()
+}
+
+Serializer.prototype.serialize = function(obj, buf){
+  if(!Buffer.isBuffer(buf)){
+    var size = this.sizeOf(obj)
+    buf = new Buffer(size)
+  }
+
+  this.writeFunc(obj, buf, 0)
+
+  return buf
+}
+
+Serializer.prototype.string = function(varName, options){
+  var getVar = this._getVarFunc(varName, options)
+
+  var sizeOf = this.sizeOf
+  var writeFunc = this.writeFunc
+
+  //FIXME only utf8/1-byte-per-char currently works reliably
+  var encoding = options.encoding || 'utf8'
+
+  if(options.length){
+    var length = options.length
+
+    var getLength = typeof options.length === 'number' ?
+                      function(obj){ return length } :
+                    typeof options.length === 'string' ?
+                      function(obj){ return obj[length] } :
+                      function(obj){ return length(obj) } //assume it's a function
+
+    this.sizeOf = options.zeroTerminated ?
+                    function(obj){
+                      return Math.min(getLength(obj), Buffer.byteLength(getVar(obj), encoding) + 1) + sizeOf(obj)
+                    } :
+                    function(obj){
+                      return getLength(obj) + sizeOf(obj)
+                    }
+
+    var addZeros =
+            options.zeroTerminated ?
+              function(written, len){
+                return written < len ? 1 : 0
+              } :
+            options.stripNull ?
+              function stripNull(written, len){
+                return len - written
+              } : function(val, len){
+                return 0
+              }
+
+    this.writeFunc = function(obj, buf){
+      var offset = writeFunc(obj, buf)
+      var len = getLength(obj)
+      var written = buf.write(getVar(obj), offset, len, encoding)
+      var toAdd = addZeros(written, len)
+
+      for(var i = 0; i < toAdd; i++){
+        buf[offset + written + i] = 0
+      }
+
+      return offset + written + toAdd
+    }
+
+    return this
+  }
+
+  if(options.zeroTerminated){
+    this.sizeOf = function(obj){
+      return Buffer.byteLength(getVar(obj), encoding) + 1 + sizeOf(obj)
+    }
+
+    this.writeFunc = function(obj, buf){
+      var offset = writeFunc(obj, buf)
+      var val = getVar(obj)
+
+      return offset + buf.write(val + '\0', offset, val.length + 1, encoding)
+    }
+
+    return this
+  }
+
+  throw new Error('.string() needs either a length or a zero-terminated string')
+}
+
+Serializer.prototype.nest = function(varName, options){
+  var getVar = this._getVarFunc(varName, options)
+
+  var sizeOf = this.sizeOf
+  var writeFunc = this.writeFunc
+
+  var type = getType(options.type)
+
+  var typeSize = type.sizeOf
+  var typeWrite = type.writeFunc
+
+  this.sizeOf = function(obj){
+    return typeSize(getVar(obj)) + sizeOf(obj)
+  }
+
+  this.writeFunc = function(obj, buf){
+    var offset = writeFunc(obj, buf)
+    return offset + typeWrite(getVar(obj), buf.slice(offset))
+  }
+
+  return this
+}
+
+Serializer.prototype.array = function(varName, options){
+  //TODO check if passed array has acceptable length
+  var getVar = this._getVarFunc(varName, options)
+
+  if(typeof options.key === "string"){
+    var plainVar = getVar
+    getVar = function(obj){
+      var val = plainVar(obj)
+      return Object.keys(val).map(function(key){
+        if(val[key][options.key] !== key) throw new Error('invalid mapping')
+        return val[key]
+      })
+    }
+  }
+
+  var sizeOf = this.sizeOf
+  var writeFunc = this.writeFunc
+
+  var type = getType(options.type)
+
+  var typeSize = type.sizeOf
+  var typeWrite = type.writeFunc
+
+  this.sizeOf = function(obj){
+    var arr = getVar(obj)
+    var sum = 0
+    for(var i = 0; i < arr.length; i++){
+      sum += typeSize(arr[i])
+    }
+    return sum + sizeOf(obj)
+  }
+
+  this.writeFunc = function(obj, buf){
+    var offset = writeFunc(obj, buf)
+    var arr = getVar(obj)
+    for(var i = 0; i < arr.length; i++){
+      offset += typeWrite(arr[i], buf.slice(offset))
+    }
+    return offset
+  }
+
+  return this
+}
+
+Serializer.prototype.choice = function(varName, options){
+  var getVar = this._getVarFunc(varName, options)
+
+  var sizeOf = this.sizeOf
+  var writeFunc = this.writeFunc
+
+  var getTag = getGetVarFunc(options.tag)
+
+  var choices = options.choices
+  var choiceTypes = {}
+
+  for(var key in choices){
+    choiceTypes[key] = getType(choices[key])
+  }
+
+  var defaultChoice = options.defaultChoice && getType(options.defaultChoice)
+
+  var getChoice = options.defaultChoice ?
+                  function(obj){
+                    var key = getTag(obj)
+                    return key in choiceTypes ? choiceTypes[key] : defaultChoice
+                  } :
+                  function(obj){
+                    var choice = choiceTypes[getTag(obj)]
+                    if(!choice) throw new Error('invalid choice')
+                    return choice
+                  }
+
+  this.sizeOf = function(obj){
+    return getChoice(obj).sizeOf(getVar(obj)) + sizeOf(obj)
+  }
+
+  this.writeFunc = function(obj, buf){
+    var offset = writeFunc(obj, buf)
+    return offset + getChoice(obj).writeFunc(getVar(obj), buf.slice(offset))
+  }
+
+  return this
+}
+
+Serializer.prototype.buffer = function(varName, options){
+  //TODO check for length
+  var getVar = this._getVarFunc(varName, options)
+
+  var sizeOf = this.sizeOf
+  var writeFunc = this.writeFunc
+
+  this.sizeOf = function(obj){
+    return getVar(obj).length + sizeOf(obj)
+  }
+
+  this.writeFunc = function(obj, buf){
+    var offset = writeFunc(obj, buf)
+    return offset + getVar(obj).copy(buf, offset)
+  }
+
+  return this
+}
+
 Object.keys(PRIMITIVE_TYPES).forEach(function(primitiveName){
+  var writeKey = 'write' + primitiveName
   var primitiveSize = PRIMITIVE_TYPES[primitiveName]
 
   Serializer.prototype[primitiveName.toLowerCase()] = function(varName, options){
-    this._addVar(varName)
-
     var getVar = this._getVarFunc(varName, options)
 
     //add the size of the primitive
@@ -113,16 +277,22 @@ Object.keys(PRIMITIVE_TYPES).forEach(function(primitiveName){
     }
 
     var writeFunc = this.writeFunc
-    this.writeFunc = function(obj, buf, offset){
-      var offset = writeFunc(obj, buf, offset + primitiveSize);
-      return offset + buf['write' + primitiveName](getVar(obj), offset)
+    this.writeFunc = function(obj, buf){
+      var offset = writeFunc(obj, buf)
+      return buf[writeKey](getVar(obj), offset)
     }
+
+    return this
   }
 })
 
 Serializer.prototype._getVarFunc = function(varName, options) {
+  //to ensure getting a value is the same while reading & serializing
+  if(this.vars.has(varName)) throw new Error('duplicated var name')
+  this.vars.add(varName)
+
   if(options && options.formatter && !options.deformatter){
-    //TODO throw new Error('formats need to be reversible')
+    throw new Error('formats need to be reversible')
   }
 
   if(options && options.deformatter){
@@ -133,14 +303,5 @@ Serializer.prototype._getVarFunc = function(varName, options) {
     }
   }
 
-  return function(obj){
-    if(!(varName in obj)) throw new Error('var not found')
-    return obj[varName]
-  }
-}
-
-//to ensure getting a value is the same while reading & serializing
-Serializer.prototype._addVar = function(varName){
-  if(this.vars.has(varName)) throw new Error('duplicated var name')
-  this.vars.add(varName)
+  return getGetVarFunc(varName)
 }
